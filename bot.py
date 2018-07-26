08c83d0d8979119a53b9e4e43579985223271d33
joel-g <joelaguerra1@gmail.com>
2018-07-25 18:01:50 -0700

    adds replied to helper functions

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import tweepy, time
 
+REPLIED_TO = 'replied_to_log.txt'
+
 # Create a config.ini file and save your Twitter API keys on lines 1-4
 with open('config.ini','r') as config:
   tokens = config.readlines()
@@ -16,17 +18,29 @@ def authenticate_twitter():
   print('Twitter authenticated.\n')
   return twitter
 
+# Helper functions you may or may not need depending on what your bot does
+
+  # If your bot replies to tweets you may want to record that you've replied to a paticular tweet in a local log so you don't reply to that tweet again
+def record_replied_to(tweet):
+    with open(replied_to, 'a') as f:
+        f.write(str(tweet.id) + '\n')
+
+  # Checks your logs to see if you've already replied to this tweet.
+def is_replied_to(tweet):
+     with open(REPLIED_TO, 'r') as f:
+        if str(tweet.id) in f.read():
+            return True
+        else:
+            return False
+
 
 # Define some functions here to do whatever the heck it is your bot is supposed to do.
 
+
 def tweet(twitter, text):
-  #you can tweet with a photo
+  #you can tweet with a photo or just text by using one of the two following lines
   twitter.update_with_media("photo.png", text)
-  
-  #you can tweet just text
   twitter.update_status(text)
-
-
 
 def main():
     twitter = authenticate_twitter()
